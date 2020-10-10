8f2e2cf2a0afbfbb415cb94466444409a66fbf7e
chi-hao.lay@outlook.com <development@cschlay.com>
2020-10-10 11:02:30 +0300

    Add Docker conf and hello world

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+}
+
+func main() {
+	handleRequests()
+	fmt.Println("Server up at localhost:8000.")
+	http.ListenAndServe(":8000", nil)
+}
